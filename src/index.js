86a8592a779bb435b49fb4a986503b5661ea6bde
coryc <cory@kreatednine.com>
2017-03-01 00:19:11 -0500

    removed some test code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-import Hello from './hello';
 
 render(
-    <Hello name="world"/>,
+    <p>Hello, <strong>World!</strong><p>,
     document.getElementById('app')
 );
